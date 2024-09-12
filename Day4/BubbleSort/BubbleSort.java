99ed5e9e3f2aec02979af96c7e07f53bf8bc8563
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-09-12 22:59:30 +0530

    Fixed Some bug in bubble sort

--- Day4/BubbleSort/BubbleSort.java
+++ Day4/BubbleSort/BubbleSort.java
@@ -7,7 +7,8 @@ public class BubbleSort {
 
     public static void bubbleSort(int[] arr) {
         int n = arr.length;
-        for (int i = 0; i < n-1; i++) {
+        System.out.println(n);
+        for (int i = 0; i <= n-1; i++) {
             for (int j = 0; j < n - i-1; j++) {
                 if (arr[j] > arr[j + 1]) {
                     // Swap arr[j] and arr[j+1]
