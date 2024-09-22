a3806e1e6ea1fb482c92fd632045cdceab126a11
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-09-22 23:56:37 +0530

    Revised BubbleSort

--- Day4/BubbleSort/BubbleSort.java
+++ Day4/BubbleSort/BubbleSort.java
@@ -11,7 +11,7 @@ public class BubbleSort {
         for (int i = 0; i <= n-1; i++) {
             for (int j = 0; j < n - i-1; j++) {
                 if (arr[j] > arr[j + 1]) {
-                    // Swap arr[j] and arr[j+1]
+                    
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
