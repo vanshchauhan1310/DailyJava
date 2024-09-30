b04597dfc73d42769908fc4382d34967975b5e28
vanshchauhan1310 <vanshchauhan1310@gmail.com>
2024-09-30 14:30:00 +0530

    added time complexity

---
+++ BasicsJava/TimeComplexity.java
@@ -0,0 +1,9 @@
+public class TimeComplexity {
+    
+
+    // if number of statement are 10^7 => then it will take 1 sec to compile 
+    // if number of statement are 10^8 => then it will take 10 sec to compile
+    // if number of statement are 10^9 => then it will take 100 sec to compile
+    // we can use binary Search if the array is sorted and max constraint is 10^10
+    // always Decide which Algorithm to be used based on the max constant which has been given to us 
+}
